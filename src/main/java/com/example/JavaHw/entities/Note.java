3018580c8700d66c24a57123084e120c445e2d08
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-09-23 22:03:02 +0300

    Update Note.java

--- src/main/java/com/example/JavaHw/entities/Note.java
+++ src/main/java/com/example/JavaHw/entities/Note.java
@@ -1,18 +1,25 @@
 package com.example.JavaHw.entities;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.ToString;
 
 @Data
 @NoArgsConstructor
-@ToString
+@Entity
+@Table(name = "Note")
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column
     private String title;
+    @Column
     private String content;
 
-    public Note(String title, String content) {
+    public Note(@NonNull String title, @NonNull String content) {
         this.title = title;
         this.content = content;
     }
