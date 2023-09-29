af89d47c445be99f2b9d58b4750a6969132c1fb4
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-09-29 18:14:42 +0300

    Update WebSecurityConfig.java

--- src/main/java/com/example/JavaHw/configs/WebSecurityConfig.java
+++ src/main/java/com/example/JavaHw/configs/WebSecurityConfig.java
@@ -2,55 +2,28 @@ package com.example.JavaHw.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends SecurityConfigurerAdapter {
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User
-                .withUsername("user")
-                .password(passwordEncoder().encode("default"))
-                .build();
-
-
-        return new InMemoryUserDetailsManager(user);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
+public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(login -> {
-                    login.loginPage("/login");
-                    login.defaultSuccessUrl("/note/list");
-                })
-                .logout(logout -> {
-                    logout.logoutSuccessUrl("/login");
-                    logout.deleteCookies();
-                }).authorizeHttpRequests(authorise -> {
-                    authorise.requestMatchers("/login").permitAll();
+                .authorizeHttpRequests(authorise -> {
+                    authorise.requestMatchers("/").permitAll();
                     authorise.anyRequest().authenticated();
-                });
+        });
         return http.build();
     }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.httpBasic(Customizer.withDefaults());
+//        return httpSecurity.build();
+//    }
 
 
 
